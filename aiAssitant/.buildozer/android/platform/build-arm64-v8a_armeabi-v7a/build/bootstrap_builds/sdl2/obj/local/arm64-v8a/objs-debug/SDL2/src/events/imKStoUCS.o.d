d2c4ef0609270bfbb47a68a37931af39ac67a320
kimjunhee <manuna530@gmail.com>
2024-12-28 06:11:32 +0900

    first commit

---
+++ aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2/src/events/imKStoUCS.o.d
@@ -0,0 +1,32 @@
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2/src/events/imKStoUCS.o: \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/events/imKStoUCS.c \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/events/../SDL_internal.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/events/../dynapi/SDL_dynapi.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/events/../SDL_internal.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/events/../dynapi/SDL_dynapi.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h:
---
+++ aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/dsp/alpha_processing_sse41.o.d
@@ -0,0 +1,8 @@
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/dsp/alpha_processing_sse41.o: \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/alpha_processing_sse41.c \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
+  /mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
+
+/mnt/c/workspace/bootcamp/aiAssitant/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
